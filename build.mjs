d5b10e608669b3490f21bcf2a82044573479f601
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-03-14 20:17:44 +0100

    updates

--- build.mjs
+++ build.mjs
@@ -1,31 +1,63 @@
-import { rollup } from "rollup";
 import { readFile, writeFile, readdir } from "fs/promises";
 import { createHash } from "crypto";
+
+import { rollup } from "rollup";
 import esbuild from "rollup-plugin-esbuild";
-import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import swc from "@swc/core";
+
+/** @type import("rollup").InputPluginOption */
+const plugins = [
+    nodeResolve(),
+    commonjs(),
+    {
+        name: "swc",
+        async transform(code, id) {
+            const result = await swc.transform(code, {
+                filename: id,
+                jsc: {
+                    externalHelpers: true,
+                    parser: {
+                        syntax: "typescript",
+                        tsx: true,
+                    },
+                },
+                env: {
+                    targets: "defaults",
+                    include: [
+                        "transform-classes",
+                        "transform-arrow-functions",
+                    ],
+                },
+            });
+            return result.code;
+        },
+    },
+    esbuild({ minify: true }),
+];
 
 for (let plug of await readdir("./plugins")) {
     const manifest = JSON.parse(await readFile(`./plugins/${plug}/manifest.json`));
-    const outName = manifest.main.split("/").reverse()[0];
-    const outPath = `./dist/${plug}/${outName}`;
+    const outPath = `./dist/${plug}/index.js`;
 
     try {
         const bundle = await rollup({
             input: `./plugins/${plug}/${manifest.main}`,
             onwarn: () => {},
-            plugins: [
-                nodeResolve(),
-                commonjs(),
-                esbuild({
-                    target: "esnext",
-                    minify: true,
-                })
-            ],
+            plugins,
         });
     
         await bundle.write({
             file: outPath,
+            globals(id) {
+                if (id.startsWith("@vendetta")) return id.substring(1).replace(/\//g, ".");
+                const map = {
+                    react: "window.React",
+                };
+
+                return map[id] || null;
+            },
             format: "iife",
             compact: true,
             exports: "named",
@@ -34,7 +66,7 @@ for (let plug of await readdir("./plugins")) {
     
         const toHash = await readFile(outPath);
         manifest.hash = createHash("sha256").update(toHash).digest("hex");
-        manifest.main = outName;
+        manifest.main = "index.js";
         await writeFile(`./dist/${plug}/manifest.json`, JSON.stringify(manifest));
     
         console.log(`Successfully built ${manifest.name}!`);
