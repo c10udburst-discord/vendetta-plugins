9e018eb8c1de05a25d8c1360e1df010782d90fb2
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-03-18 11:38:00 +0100

    create webhooks

---
+++ plugins/create-webhooks/src/index.tsx
@@ -0,0 +1,47 @@
+import { findByDisplayName, findByProps } from "@vendetta/metro";
+import { stylesheet, React, ReactNative, i18n } from "@vendetta/metro/common";
+import { instead} from "@vendetta/patcher";
+
+let patches = [];
+
+const {View} = ReactNative;
+
+const ConnectedWebhooksOverview = findByDisplayName("ConnectedWebhooksOverview", false);
+const Button = findByDisplayName("Button", false).default;
+const { create: createWebhook } = findByProps("update", "create", "fetchForChannel");
+const {getChannel} = findByProps("getChannel");
+
+const Styles = stylesheet.createThemedStyleSheet({
+    'container': {
+        'flex': 1,
+        'paddingBottom': 32
+    },
+    'button': {
+        'maxHeight': 32,
+        'margin': 16
+    }
+})
+
+function onLoad() {   
+    patches.push(instead("default", ConnectedWebhooksOverview, (args, orig) => {
+        const channel = getChannel(args[0]?.channelId)
+        return (<View style={Styles.container}>
+            {orig?.(...args)}
+            <Button color="brand" style={Styles.button}
+                text={i18n?.Messages?.WEBHOOK_CREATE || "Create Webhook"}
+                onPress={() => {
+                    createWebhook?.(channel.guild_id, channel.id)
+                }}
+            />
+        </View>)
+    }))
+}
+
+export default {
+    onLoad,
+    onUnload: () => {
+        for (const unpatch of patches) {
+            unpatch();
+        };
+    }
+}
