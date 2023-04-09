20e56d3b43200e4acdc531a82c40a165eeea82f0
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-04-09 13:39:11 +0200

    le https://github.com/aeongdesu/vdplugins/commit/df0e34002c220722e7e465cbad0c0ad0bca79a97

--- plugins/delete-embeds/src/index.tsx
+++ plugins/delete-embeds/src/index.tsx
@@ -2,6 +2,7 @@ import { findByName, findByProps } from "@vendetta/metro";
 import { i18n, constants } from "@vendetta/metro/common";
 import { after, before} from "@vendetta/patcher";
 import { Forms } from "@vendetta/ui/components";
+import { React } from "@vendetta/metro/common";
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
 
 let patches = [];
@@ -21,21 +22,22 @@ function onLoad() {
         if (args != "MessageLongPressActionSheet") return;
         component.then(instance => {
             const unpatch = after("default", instance, (_, component) => {
-                const [msgProps, oldButtons] = component.props?.children?.props?.children?.props?.children
+                React.useEffect(() => () => { unpatch() }, []) // omg!!!!!!!!!!!!!
+                const [msgProps, buttons] = component.props?.children?.props?.children?.props?.children
     
                 const message = msgProps?.props?.message ?? actionMessage?.message
     
-                if (!oldButtons || !message) return
+                if (!buttons || !message) return
 
                 const channel = getChannel(message.channel_id)
                 if (message.embeds.length == 0 || (getCurrentUser().id !== message.author.id && !Permissions.can(constants.Permissions.MANAGE_MESSAGES, channel))) {
-                    unpatch()
+                    //unpatch()
                     return
                 }
 
                 const label = i18n?.Messages?.WEBHOOK_DELETE_TITLE?.intlMessage?.format({name:"Embed"})
 
-                component.props.children.props.children.props.children[1] = [...oldButtons,
+                buttons.push(
                 <FormRow
                     label={label || "Delete Embed"}
                     leading={<Icon source={getAssetId("ic_close_16px")} />}
@@ -43,8 +45,7 @@ function onLoad() {
                         suppressEmbeds(message.channel_id, message.id)
                         ActionSheet.hideActionSheet()
                     }}
-                />]
-                unpatch()
+                />)
             })
         })
     }));
